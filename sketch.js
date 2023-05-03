03d29675f95245e6b00de361062e76dd88dde5b8
joshdrewthis <72427638+joshdrewthis@users.noreply.github.com>
2023-05-03 05:22:14 -0700

    updated site

---
+++ sketch.js
@@ -0,0 +1,35 @@
+let circles = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER);
+  
+}
+
+function draw() {
+  background(225);
+
+  //circles
+  for (let i = 0; i < circles.length; i++) {
+    let circle = circles[i];
+    fill(circle.color);
+    noStroke();
+    ellipse(circle.x, circle.y, circle.size, circle.size);
+  }
+
+  //add when the mouse is pressed
+  if (mouseIsPressed) {
+    let circle = {
+      x: mouseX,
+      y: mouseY,
+      size: random(10, 50),
+      color: color(random(255), random(255), random(255), 100)
+    };
+    circles.push(circle);
+  }
+
+  //remove circles when the 'c' key is pressed
+  if (keyIsPressed && key === 'c') {
+    circles.splice(0, 1);
+  }
+}
